e2199e8f43705952c5185b5ab17fddaa6f6cef27
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-05-15 21:43:36 +0100

    Clean up apps

--- congentodb_client/apps/links.py
+++ congentodb_client/apps/links.py
@@ -2,15 +2,14 @@ from pyforms_web.basewidget import BaseWidget
 
 
 class ExternalLinkBaseWidget(BaseWidget):
-    ORQUESTRA_MENU = 'bottom-left'
+    ORQUESTRA_MENU = "bottom-left"
     ORQUESTRA_MENU_ORDER = 900
-    ORQUESTRA_MENU_ICON = 'external alternate'
-
+    ORQUESTRA_MENU_ICON = "external alternate"
 
 
 class CongentoDbLink(ExternalLinkBaseWidget):
-    UID = 'congentodb-link'
-    TITLE = 'Congento DB'
+    UID = "congentodb-link"
+    TITLE = "Congento DB"
 
-    ORQUESTRA_URL = 'https://congento.org/'
-    ORQUESTRA_TARGET = '_blank'
+    ORQUESTRA_URL = "https://congento.org/"
+    ORQUESTRA_TARGET = "_blank"
--- congentodb_client/apps/sync_app.py
+++ congentodb_client/apps/sync_app.py
@@ -6,51 +6,46 @@ from pyforms_web.allcontrols import ControlButton
 
 class SyncApp(ModelAdminWidget):
 
-    UID = 'sync-with-congentodb'
+    UID = "sync-with-congentodb"
     MODEL = MissedSync
-    TITLE = 'Sync with Congento DB'
+    TITLE = "Sync with Congento DB"
 
-
-    LIST_DISPLAY = ['contenttype', 'object_id', 'operation', 'created']
+    LIST_DISPLAY = ["contenttype", "object_id", "operation", "created"]
 
     LAYOUT_POSITION = conf.ORQUESTRA_HOME
-    ORQUESTRA_MENU = 'middle-left'
+    ORQUESTRA_MENU = "middle-left"
     ORQUESTRA_MENU_ORDER = 10
-    ORQUESTRA_MENU_ICON = 'database'
+    ORQUESTRA_MENU_ICON = "database"
 
-    LIST_HEADERS = ['Table', 'object id', 'operation', 'created on']
+    LIST_HEADERS = ["Table", "object id", "operation", "created on"]
 
     def __init__(self, *args, **kwargs):
 
-        self._sync_btn = ControlButton('Syncronize data with the remote CongentoDb', default=self.__sync_evt, css='red')
+        self._sync_btn = ControlButton(
+            "Syncronize data with the remote Congento DB",
+            default=self.__sync_evt,
+            css="red",
+        )
 
         super().__init__(*args, **kwargs)
 
         self._list.item_selection_changed_event = self.dummy
 
-
-        self.formset = [
-            '_sync_btn',
-            '_list'
-        ]
-
+        self.formset = ["_sync_btn", "_list"]
 
     def dummy(self):
         pass
-
-
 
     def get_queryset(self, request, queryset):
         return queryset.filter(committed=None)
 
     def __sync_evt(self):
 
-        for o in MissedSync.objects.filter(committed=None).order_by('created'):
+        for o in MissedSync.objects.filter(committed=None).order_by("created"):
             o.sync()
             print(o)
 
         self.populate_list()
-
 
     def has_add_permissions(self):
         return False
