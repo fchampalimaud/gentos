7165b577f13b1e548935c3dd390eb822458f8082
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-05-15 21:11:03 +0100

    Configure static settings and urls

--- conf/settings.py
+++ conf/settings.py
@@ -164,17 +164,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static", "img"),
-    os.path.join(BASE_DIR, "static", "images"),
-    os.path.join(BASE_DIR, "static", "css"),
-    # os.path.join(BASE_DIR,'static', "js"),
-)
 
 
 SITE_ID = 1  # Required for allauth module
