b8fd2eeb1838aad9332eceb0f0927ae780184d29
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-13 16:36:20 +0100

    Update dependencies

--- conf/settings.py
+++ conf/settings.py
@@ -124,8 +124,8 @@ DATABASES = {
         "NAME": os.getenv("DATABASE_API_NAME"),
         "USER": os.getenv("DATABASE_API_USERNAME"),
         "PASSWORD": os.getenv("DATABASE_API_PASSWORD"),
-        "AUTH": "rest_models.backend.auth.BasicAuth",
-        "PREVENT_DISTINCT": False,
+        # "AUTH": "rest_models.backend.auth.BasicAuth",
+        # "PREVENT_DISTINCT": False,
     },
 }
 
