1c14c02535fdb37fcfc28c9dc9784e876aa2ebda
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-18 15:30:52 +0100

    Add django_extensions to installed apps

--- conf/settings.py
+++ conf/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
 ]
 
 if DEBUG:
-    INSTALLED_APPS += ["debug_toolbar"]
+    INSTALLED_APPS += ["debug_toolbar", "django_extensions"]
     MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
     INTERNAL_IPS = ["127.0.0.1"]
 
