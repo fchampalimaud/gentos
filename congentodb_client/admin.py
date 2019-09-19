06ff97f83ee5c824b4c02b8dde72cd31ab3446b6
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-19 18:10:58 +0100

    Register MissedSync in admin

--- congentodb_client/admin.py
+++ congentodb_client/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Rodent, Fly, Fish
+from .models import Rodent, Fly, Fish, MissedSync
 
 admin.site.register(Rodent)
 admin.site.register(Fly)
 admin.site.register(Fish)
+admin.site.register(MissedSync)
