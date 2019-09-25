affa51945e112a26305f1d97d7f2ffe2a37c24a7
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-25 14:36:51 +0100

    Update Rodent sync model

--- congentodb_client/models/rodent.py
+++ congentodb_client/models/rodent.py
@@ -21,14 +21,14 @@ class Rodent(models.Model):
     origin = models.CharField(max_length=40)
     origin_other = models.CharField(max_length=40, blank=True)
     category = models.CharField(max_length=40)
-    background = models.CharField(max_length=80, null=True, blank=True)
+    background = models.CharField(max_length=80, blank=True)
     zygosity = models.CharField(max_length=20, blank=True)
     line_description = models.TextField(blank=True)
-    coat_color = models.CharField(max_length=40, null=True, blank=True)
-    reporter_gene = models.CharField(max_length=40, null=True, blank=True)
-    inducible_cassette = models.CharField(max_length=40, null=True, blank=True)
+    coat_color = models.CharField(max_length=40, blank=True)
+    reporter_gene = models.CharField(max_length=40, blank=True)
+    inducible_cassette = models.CharField(max_length=40, blank=True)
 
-    remote_id = models.BigIntegerField("Remote id")
+    remote_id = models.BigIntegerField("Remote ID")
 
     # the only customisation that makes this model special
     class APIMeta:
