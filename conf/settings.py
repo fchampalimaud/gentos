be341b27001c436b8a2cec3bdda201aa33febe4c
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2020-02-03 16:59:59 +0000

    move project wide static files

--- conf/settings.py
+++ conf/settings.py
@@ -168,9 +168,8 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
 
 # E-mail
